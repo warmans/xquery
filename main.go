2f2fa0631696ce60a7cbcdcff88ee1470cf1a387
warmans <stefan.warman@switchconcepts.com>
2015-01-21 22:14:28 +0000

    Improved error handling, outputs, added config

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/go-martini/martini"
+	"github.com/spf13/viper"
 )
 
 import _ "github.com/go-sql-driver/mysql"
@@ -25,9 +26,16 @@ type QueryRunner struct {
 	Hosts map[string]string
 }
 
+type Result struct {
+	Host   string
+	Record []string
+	Err    error
+	Done   bool
+}
+
 func (qr *QueryRunner) Execute(query *Query, responseWriter io.Writer) {
 
-	resultChannel := make(chan []string)
+	resultChannel := make(chan Result)
 
 	for name, dsn := range qr.Hosts {
 		go qr.runQueryOnHost(name, dsn, query, resultChannel)
@@ -36,16 +44,24 @@ func (qr *QueryRunner) Execute(query *Query, responseWriter io.Writer) {
 	//write CSV to response stream
 	c := csv.NewWriter(responseWriter)
 
+	//capture errors and emit only after all data has been returned
+	errorResults := []Result{}
+
 	timeout := false
 	todo := len(qr.Hosts)
 
 	for timeout != true && todo > 0 {
 		select {
 		case row := <-resultChannel:
-			if row == nil {
-				todo--
+			if row.Err != nil || row.Done == true {
+
+				todo-- //failed or complete so stop waiting
+
+				if row.Err != nil {
+					errorResults = append(errorResults, row)
+				}
 			} else {
-				c.Write(row)
+				c.Write(append([]string{row.Host}, row.Record...))
 			}
 		case <-time.After(time.Second * 10): //todo: implement actual timeout member of Query
 			timeout = true
@@ -53,21 +69,42 @@ func (qr *QueryRunner) Execute(query *Query, responseWriter io.Writer) {
 			time.Sleep(time.Millisecond)
 		}
 	}
+
+	if len(errorResults) > 0 {
+		c.Write([]string{""})
+		c.Write([]string{"DATA ENDS! ERRORS FOLLOW:"})
+		for _, errorRow := range errorResults {
+			c.Write([]string{errorRow.Host, fmt.Sprintf("%s", errorRow.Err)})
+		}
+	}
+
 	c.Flush()
 }
 
-func (qr *QueryRunner) runQueryOnHost(name string, dsn string, query *Query, resultChannel chan []string) {
+func (qr *QueryRunner) runQueryOnHost(name string, dsn string, query *Query, resultChannel chan Result) {
 
+	//open connection
 	db, err := sql.Open("mysql", dsn)
-	checkError(err)
+	if err != nil {
+		resultChannel <- Result{Host: name, Err: err}
+		return
+	}
 	defer db.Close()
 
+	//run query
 	rows, err := db.Query(query.Sql)
-	checkError(err)
+	if err != nil {
+		resultChannel <- Result{Host: name, Err: err}
+		return
+	}
 	defer rows.Close()
 
+	//get result columns
 	columns, err := rows.Columns()
-	checkError(err)
+	if err != nil {
+		resultChannel <- Result{Host: name, Err: err}
+		return
+	}
 
 	scanArgs := make([]interface{}, len(columns))
 	values := make([]interface{}, len(columns))
@@ -76,9 +113,13 @@ func (qr *QueryRunner) runQueryOnHost(name string, dsn string, query *Query, res
 		scanArgs[i] = &values[i]
 	}
 
+	//start processing result
 	for rows.Next() {
 		err = rows.Scan(scanArgs...)
-		checkError(err)
+		if err != nil {
+			resultChannel <- Result{Host: name, Err: err}
+			return
+		}
 
 		record := make([]string, len(columns))
 
@@ -88,10 +129,11 @@ func (qr *QueryRunner) runQueryOnHost(name string, dsn string, query *Query, res
 			}
 		}
 
-		resultChannel <- record
+		resultChannel <- Result{Host: name, Record: record}
 	}
 
-	resultChannel <- nil
+	//done
+	resultChannel <- Result{Done: true}
 }
 
 type Query struct {
@@ -101,16 +143,19 @@ type Query struct {
 
 func main() {
 
-	hosts := make(map[string]string)
-	hosts["localhost1"] = "root:@/xquery"
-	hosts["localhost2"] = "root:@/xquery"
-	hosts["localhost3"] = "root:@/xquery"
-	hosts["localhost4"] = "root:@/xquery"
-
 	m := martini.Classic()
 
-	//services
-	m.Map(&QueryRunner{Hosts: hosts})
+	//config
+
+	config := viper.New()
+	config.SetConfigName("xquery")
+	config.AddConfigPath("./config")
+	config.ReadInConfig()
+
+	m.Map(&config)
+
+	//query runner
+	m.Map(&QueryRunner{Hosts: config.GetStringMapString("dbs")})
 
 	//routes
 	m.Post("/query", func(res http.ResponseWriter, req *http.Request, qr *QueryRunner) {
